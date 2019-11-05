c0dcb8ade616eb4de9c756b10abf49d202a184d3
Alex <dergachev88@gmail.com>
2019-11-05 13:42:24 +0300

    draw wire in separate function

--- AnimateScheme.py
+++ AnimateScheme.py
@@ -6,6 +6,7 @@ image_height = 140
 resistor_length = 50
 resistor_width = 20
 resistor_outline_width = 3
+wire_outline_width = 1
 
 
 def c_x(x):
@@ -23,11 +24,29 @@ def draw_resistor(draw, x_center, y_center):
         outline=(0, 0, 0), width=resistor_outline_width)
 
 
+def draw_wire(draw, xy):
+    if not xy:
+        return
+
+    xy_new = xy
+    if not isinstance(xy_new[0], tuple):
+        xy_new = [(xy[i], xy[i+1]) for i in range(0, len(xy), 2)]
+    xy_new = [(c_x(elem[0]), c_y(elem[1])) for elem in xy_new]
+
+    draw.line(xy_new, wire_outline_width)
+
+
 def create_frame_stage_1(step, maxsteps):
     img = Image.new('RGB', (image_width, image_height), (255, 255, 255))
     draw = ImageDraw.Draw(img)
 
     draw_resistor(draw, 0, 0)
+    draw_resistor(draw, -90, 0)
+    draw_resistor(draw, 90, 0)
+
+    draw_wire(draw, [25, 0, 65, 0])
+    draw_wire(draw, [(-25, 0,), (-65, 0)])
+    draw_wire(draw, [])
 
     return img
 
