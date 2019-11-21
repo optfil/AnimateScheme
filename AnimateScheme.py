1490dec231c3f877964054205d571eb486022506
Alex <dergachev88@gmail.com>
2019-11-21 03:50:58 +0300

    svg defs demo

--- AnimateScheme.py
+++ AnimateScheme.py
@@ -5,7 +5,15 @@ from svgwrite import *
 
 def demo():
     dwg = Drawing()
-    square = dwg.add(dwg.rect(insert=(0, 0), size=(50, 20), style='fill:none; stroke:red; stroke-width:2'))
+    symbol = dwg.defs.add(dwg.symbol(id="resistor", viewBox="0 0 50 20",
+                                     style="fill:none; stroke:black; stroke-width:8"))
+    symbol.add(dwg.rect(insert=(0, 0), size=(50, 20)))
+
+    dwg.add(dwg.use(href='#resistor', insert=(100, 100), size=(50, 20)))
+    dwg.add(dwg.use(href='#resistor', insert=(200, 100), size=(50, 20)))
+    dwg.add(dwg.line(start=(50, 110), end=(100, 110), style='stroke:black; stroke-width:4'))
+    dwg.add(dwg.line(start=(150, 110), end=(200, 110), style='stroke:black; stroke-width:4'))
+    dwg.add(dwg.line(start=(250, 110), end=(300, 110), style='stroke:black; stroke-width:4'))
     dwg.write(open('tmp.svg', 'w', encoding='utf-8'))
 
 
